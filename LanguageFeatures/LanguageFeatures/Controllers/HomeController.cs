89c22398f6c4065eab04ee4e61683a650e42329d
SirJUST <theoperaking@yahoo.com>
2019-01-31 08:48:56 -0500

    get and set properties

--- LanguageFeatures/LanguageFeatures/Controllers/HomeController.cs
+++ LanguageFeatures/LanguageFeatures/Controllers/HomeController.cs
@@ -14,5 +14,21 @@ namespace LanguageFeatures.Controllers
         {
             return "Navigate to a URL to show an example";
         }
+
+        public ViewResult AutoProperty()
+        {
+            // create new Product object
+            Product myProduct = new Product();
+
+            // set the property value
+            myProduct.Name = "Kayak";
+
+            // get the property
+            string productName = myProduct.Name;
+
+            // generate the view
+            return View("Result",
+                (object)String.Format("Product name: {0}", productName));
+        }
     }
 }
