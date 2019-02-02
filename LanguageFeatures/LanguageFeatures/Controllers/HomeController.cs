a53c06fee46e86f2044664c491df254cfabdcbc5
SirJUST <theoperaking@yahoo.com>
2019-02-01 22:31:17 -0500

    object initializer syntax

--- LanguageFeatures/LanguageFeatures/Controllers/HomeController.cs
+++ LanguageFeatures/LanguageFeatures/Controllers/HomeController.cs
@@ -30,5 +30,44 @@ namespace LanguageFeatures.Controllers
             return View("Result",
                 (object)String.Format("Product name: {0}", productName));
         }
+
+        public ViewResult CreateProduct()
+        {
+            // create a new Product object
+            //Product myProduct = new Product();
+
+            // set the property values
+            //myProduct.ProductID = 100;
+            //myProduct.Name = "Kayak";
+            //myProduct.Description = "A boat for one person";
+            //myProduct.Price = 275M;
+            //myProduct.Category = "Watersports";
+
+            // object initializer syntax
+            Product myProduct = new Product()
+            {
+                ProductID = 100,
+                Name = "Kayak",
+                Description = "A boat for one person",
+                Price = 275M,
+                Category = "Watersports"
+            };
+
+            return View("Result", (object)String.Format("Category: {0}", myProduct.Category));
+        }
+
+        public ViewResult CreateCollection()
+        {
+            string[] stringArray = { "apple", "orange", "plum" };
+
+            List<int> intList = new List<int> { 10, 20, 30, 40 };
+
+            Dictionary<string, int> myDict = new Dictionary<string, int>
+            {
+                {"apple", 10 }, {"orange", 20}, {"plum", 30}
+            };
+
+            return View("Result", (object)stringArray[1]);
+        }
     }
 }
