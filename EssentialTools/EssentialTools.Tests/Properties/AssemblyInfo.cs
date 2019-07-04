e1bd8b322cc1af728d88c25176224ebe0e32442f
SirJUST <theoperaking@yahoo.com>
2019-07-04 12:51:29 -0400

    Remove defunct project, add DI practice project

---
+++ EssentialTools/EssentialTools.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("EssentialTools.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("EssentialTools.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("309a2d70-0f88-4ef8-97d7-474b8fb09124")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ EssentialTools/EssentialTools/Models/ILinqValueCalculator.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+
+namespace EssentialTools.Models
+{
+    public interface ILinqValueCalculator
+    {
+        decimal ValueProducts(IEnumerable<Product> products);
+    }
+}
